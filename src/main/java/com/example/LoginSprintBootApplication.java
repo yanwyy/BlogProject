a66c061e861473df777fc49d18c82763d1a03107
ycf <2528991016@qq.com>
2023-06-28 22:06:00 +0800

    asjdg

--- src/main/java/com/example/LoginSprintBootApplication.java
+++ src/main/java/com/example/LoginSprintBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginSprintBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LoginSprintBootApplication.class, args);
     }
